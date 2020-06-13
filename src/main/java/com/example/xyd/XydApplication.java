72f90ba2520445a53fe9edf8824920991779ea72
171250011 <171250011@smail.nju.edu.cn>
2020-06-13 21:14:44 +0800

    first commit

---
+++ src/main/java/com/example/xyd/XydApplication.java
@@ -0,0 +1,13 @@
+package com.example.xyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XydApplication.class, args);
+	}
+
+}
